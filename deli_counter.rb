28fe84be458c2a82346483e2e1a2a137246128f3
bcarrol2 <blairacarroll@gmail.com>
2018-10-16 17:44:09 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,9 +11,8 @@ def line(x)
     puts "The line is currently: #{line_array.join(" ")}"
   end
 end
-def take_a_number(katz_deli, name)
-  katz_deli.push(name)
-  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+def take_a_number(katz_deli)
+  puts "Welcome, You are number #{katz_deli.length} in line."
 end
 def now_serving(array)
   if array.empty?
